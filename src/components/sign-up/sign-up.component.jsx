77bbdf77df8ab68bca1ef3d6ca12e988a268e869
Ivounda16 <kombilahogrel@gmail.com>
2020-04-22 14:10:54 +0000

    finishing sign in handleSubmit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -45,7 +45,7 @@ class SignUp extends React.Component {
     handleChange = event => {
         const { name, value } = event.target;
 
-        this.setState({ [name]: value })
+        this.setState({ [name]: value });
     }
 
     render() {
