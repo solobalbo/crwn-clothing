b4e85508f02ed6805d8e77ca72998a7c4524e2d6
Ivounda16 <kombilahogrel@gmail.com>
2020-04-22 15:51:25 +0000

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,25 +9,20 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null
 
 componentDidMount() {
+  const {setCurrentUser} = this.props;
   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     if (userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
 
       userRef.onSnapshot(snapShot => {
-        this.setState({
+        setCurrentUser({
           currentUser: {
             id: snapShot.id,
             ...snapShot.data()
@@ -34,7 +30,7 @@ componentDidMount() {
         });
       });
     }
-    this.setState({ currentUser: userAuth })
+    setCurrentUser({ userAuth })
   } );
 }
 
@@ -46,7 +42,7 @@ componentWillUnmount() {
   render() {
     return (
       <div>
-      <Header currentUser = {this.state.currentUser} />
+      <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage}/>
@@ -57,4 +53,8 @@ componentWillUnmount() {
   }
 }
 
-export default App;
+const mapDispatchProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/4.4 crown.svg.svg';
+import { connect } from 'react-redux';
 import  { auth } from '../../firebase/firebase.utils';
+import { ReactComponent as Logo } from '../../assets/4.4 crown.svg.svg';
+
 import './header.styles.scss'
 
 const Header = ({ currentUser }) => (
@@ -31,4 +33,7 @@ const Header = ({ currentUser }) => (
         </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+export default connect(mapStateToProps)(Header);
